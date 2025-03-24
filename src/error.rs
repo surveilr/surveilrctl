a51005659596f256a988c694041ca760940c3539
Baasit <baasisek01@gmail.com>
2025-03-24 17:06:36 +0100

    fix(app): fix lint

--- src/error.rs
+++ src/error.rs
@@ -15,8 +15,12 @@ impl Display for SurveilrCtlError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         use SurveilrCtlError::*;
         match self {
-            InvalidCommand(v) => write!(f, "The command '{}' is invalid. Please run help to see a list of available commands", v), 
-            UptError(err) => write!(f, "{err:#?}")
+            InvalidCommand(v) => write!(
+                f,
+                "The command '{}' is invalid. Please run help to see a list of available commands",
+                v
+            ),
+            UptError(err) => write!(f, "{err:#?}"),
         }
     }
 }
